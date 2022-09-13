17a6929ac8856ec02925fcbc646c171f0a1f196e
Leo Lanese <laneseleo@gmail.com>
2022-09-13 21:08:51 +0100

    more improvements

--- pages/example-4.js
+++ pages/example-4.js
@@ -1,4 +1,5 @@
 import "@fontsource/roboto";
+import {TableSortLabel} from "@mui/material";
 import Button from "@mui/material/Button";
 import Paper from '@mui/material/Paper';
 import Table from '@mui/material/Table';
@@ -8,21 +9,55 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Image from "next/future/image";
-import {useState} from "react";
+import React,{useState} from "react";
 import img from "../imgs/test.png";
 
-function createData(index = 0, tutorial = '', link = '') {
+function createData1(index = 0, tutorial = '', link = '') {
     return { index, tutorial, link }
 }
   
-const rows = [
-    createData(1,'A','1',),
-    createData(2,'B','2',),
-    createData(3,'C','3',),
+const rows1 = [
+    createData1(1,'A','1',),
+    createData1(2,'B','2',),
+    createData1(3,'C','3',),
 ]
+
+function createData2(number, item, qty, price) {
+  return { number, item, qty, price };
+}
+
+const rows2 = [
+  createData2(1, "Apple", 5, 3),
+  createData2(2, "Orange", 2, 2),
+  createData2(3, "Grapes", 3, 1),
+  createData2(4, "Tomato", 2, 1.6),
+  createData2(5, "Mango", 1.5, 4)
+];
 
 function Example3() {
   const [counter, setCounter] = useState(0);
+
+  const [rowData, setRowData] = useState(rows2);
+  const [orderDirection, setOrderDirection] = useState("asc");
+
+  const sortArray = (arr, orderBy) => {
+    switch (orderBy) {
+      case "asc":
+      default:
+        return arr.sort((a, b) =>
+          a.price > b.price ? 1 : b.price > a.price ? -1 : 0
+        );
+      case "desc":
+        return arr.sort((a, b) =>
+          a.price < b.price ? 1 : b.price < a.price ? -1 : 0
+        );
+    }
+  };
+
+  const handleSortRequest = () => {
+    setRowData(sortArray(rows2, orderDirection));
+    setOrderDirection(orderDirection === "asc" ? "desc" : "asc");
+  };
 
   return (
     <div>
@@ -75,7 +110,7 @@ function Example3() {
               </TableRow>
             </TableHead>
             <TableBody>
-              {rows.map((row, index) => (
+              {rows1.map((row, index) => (
                 <TableRow key={row.name}>
                   <TableCell component="th" scope="row">
                     {row.index}
@@ -92,6 +127,43 @@ function Example3() {
           </Table>
         </TableContainer>
       </div>
+
+      <div style={{ margin: 30 }}>
+        <hr></hr>
+        <h1></h1>
+        <TableContainer component={Paper}>
+          <Table aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell align="center">S.No</TableCell>
+
+                <TableCell align="center">Item</TableCell>
+
+                <TableCell align="center">Quantity&nbsp;(kg)</TableCell>
+
+                <TableCell align="center" onClick={handleSortRequest}>
+                  <TableSortLabel active={true} direction={orderDirection}>
+                    Price&nbsp;($)
+                  </TableSortLabel>
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rowData.map((row) => (
+                <TableRow key={row.number}>
+                  <TableCell component="th" scope="row" align="center">
+                    {row.number}
+                  </TableCell>
+                  <TableCell align="center">{row.item}</TableCell>
+                  <TableCell align="center">{row.qty}</TableCell>
+                  <TableCell align="center">{row.price}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        </div>
+        
     </div>
   );
 }
