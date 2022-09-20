5438699d4462e8896941814ba7d793b7af725a14
Leo Lanese <laneseleo@gmail.com>
2022-09-20 21:08:46 +0100

    script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import {CacheProvider} from "@emotion/react";
 import {CssBaseline,ThemeProvider} from "@mui/material";
+import Script from "next/script";
 import PropTypes from "prop-types";
 import '../styles/globals.css';
 import lightTheme from '../styles/theme/lightTheme';
@@ -12,6 +13,13 @@ const MyApp = (props: {Component: any; emotionCache?: any; pageProps: any;}) => 
 
   return (
     <>
+      <Script
+        id="bootstrap-cdn"
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        onError={(e) => {
+          console.log("error loading bootstrap.bundle: ", e);
+        }}
+      />
       <CacheProvider value={emotionCache}>
         <ThemeProvider theme={lightTheme}>
           {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
