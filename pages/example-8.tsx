bc7754cdc01ef7e5861a3a4c3298bd119b930e03
Leo Lanese <laneseleo@gmail.com>
2022-09-21 20:51:11 +0100

    readme update

--- pages/example-8.tsx
+++ pages/example-8.tsx
@@ -4,7 +4,7 @@ import {useState} from "react";
 import {films} from 'utility/films';
 
 const CustomPaper = ({ children, ...paperProps }) => (
-  <Paper elevation={8} {...paperProps} className="customAutoComplete">
+  <Paper elevation={15} {...paperProps} className="customAutoComplete">
     {children}
   </Paper>
 );
@@ -14,35 +14,34 @@ function Example8() {
   const [values, setValues] = useState([]);
 
   return (
-    <div>
-      <Stack spacing={3} sx={{ width: 500 }}>
-        <Autocomplete
-          openOnFocus
-          id={"autocomplete"}
-          disableClearable
-          multiple
-          disableCloseOnSelect
-          id="tags-standard"
-          isOptionEqualToValue={(o, v) => o.title == v.title}
-          renderInput={(params) => (
-            <TextField
-              {...params}
-              placeholder={"Select an existing facility"}
-            />
-          )}
-          onChange={(e, selected) => {
-            setValues(selected);
-          }}
-          getOptionLabel={(option) => `(${option?.year}) ${option?.title}`}
-          options={[...films]}
-          value={values}
-          groupBy={(option) => option?.year ?? ""}
-          PaperComponent={CustomPaper}
-          // componentsProps={{ paper: { elevation: 8 } }}
-        />
-      </Stack>
-      <CssBaseline />
-    </div>
+    <>
+      <div style={{ margin: 30 }}>
+        <Stack spacing={3} sx={{ width: 500 }}>
+          <Autocomplete
+            openOnFocus
+            id={"autocomplete"}
+            disableClearable
+            multiple
+            disableCloseOnSelect
+            id="tags-standard"
+            isOptionEqualToValue={(o, v) => o.title == v.title}
+            renderInput={(params) => (
+              <TextField {...params} placeholder={"Select a movie"} />
+            )}
+            onChange={(e, selected) => {
+              setValues(selected);
+            }}
+            getOptionLabel={(option) => `(${option?.year}) ${option?.title}`}
+            options={[...films]}
+            value={values}
+            groupBy={(option) => option?.year ?? ""}
+            PaperComponent={CustomPaper}
+            componentsProps={{ paper: { elevation: 12 } }}
+          />
+        </Stack>
+        <CssBaseline />
+      </div>
+    </>
   );
 }
 
