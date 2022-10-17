7833001001a7a8c47e125047efe44b7f0e4e9892
Leo Lanese <laneseleo@gmail.com>
2022-10-17 21:28:48 +0100

    improvements

--- pages/example-12.tsx
+++ pages/example-12.tsx
@@ -1,4 +1,5 @@
 import {useState} from "react";
+import {Back} from 'utility/Back';
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
@@ -7,41 +8,47 @@ export default function Home() {
   const api_url = `https://api.github.com/search/users?q=${search}&per_page=5`;
 
   return (
-    <div className={styles.main}>
-      <input
-        type="text"
-        placeholder="Search Github Users"
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-      />
+    <>
+      <div className={styles.main}>
+        <Back />
+        <h1>Dynamic imports for libraries</h1>
+        <div>
+          <input
+            type="text"
+            placeholder="Search Github Users"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
 
-      <button
-        onClick={async () => {
-          // dynamically load the axios dependency
-          const axios = (await import("axios")).default;
-          const res = await axios.get(api_url).then((res) => {
-            setResponse(res);
-          });
-        }}
-      >
-        Search for GitHub users
-      </button>
+          <button
+            onClick={async () => {
+              // dynamically load the axios dependency
+              const axios = (await import("axios")).default;
+              const res = await axios.get(api_url).then((res) => {
+                setResponse(res);
+              });
+            }}
+          >
+            Search for GitHub users
+          </button>
 
-      <div>
-        <h1>{search} Results</h1>
-        <ul>
-          {response?.data ? (
-            response &&
-            response?.data.items.map((item, index) => (
-              <span key={index}>
-                <p>{item.login}</p>
-              </span>
-            ))
-          ) : (
-            <p>No Results</p>
-          )}
-        </ul>
+          <div>
+            <h1>{search} Results</h1>
+            <ul>
+              {response?.data ? (
+                response &&
+                response?.data.items.map((item, index) => (
+                  <span key={index}>
+                    <p>{item.login}</p>
+                  </span>
+                ))
+              ) : (
+                <p>No Results</p>
+              )}
+            </ul>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
