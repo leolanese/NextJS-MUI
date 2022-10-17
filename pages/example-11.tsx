903d71161fd016efee08b39db2e134b4414003fc
Leo Lanese <laneseleo@gmail.com>
2022-10-17 20:57:57 +0100

    improvements

--- pages/example-11.tsx
+++ pages/example-11.tsx
@@ -1,24 +1,31 @@
 import dynamic from "next/dynamic";
 import {Suspense} from "react";
-import {Back} from "utility/Back";
 
 const DynamicComponent = dynamic(() => import("../pages/example-10"));
 
-const fetcher = (url: string)  => fetch(url).then((res) => res.json());
+const fetcher = (url: string) => 
+  fetch(url).then((res) => res.json());
+
+const Example9 = dynamic(() =>
+  import("./example-9").then((res) => res.default)
+);
 
 export default function Index() {
 
   return (
     <>
       <div style={{ margin: 30 }}>
-        <Back />
         <h1>Dynamic imports</h1>
         <Suspense fallback={<div>Loading...</div>}>
           {/* react/suspense has a specified fallback element, which is displayed 
-          until the imported component is available: Faster page loads and Low bounce rates */}
+          until the imported component is available */}
+          {/* Dynamic imports: Faster page loads and Low bounce rates */}
+          <DynamicComponent />
         </Suspense>
-        <DynamicComponent />
-        <p>HOME PAGE is here! </p>
+
+        <Suspense fallback={<div>Loading...</div>}>
+          <Example9 />
+        </Suspense>
       </div>
     </>
   );
